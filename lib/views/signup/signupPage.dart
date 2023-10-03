2ef8d6ad01308b478a6934bb6e1880375a2a3622
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-10-03 18:56:52 +0545

    loginpageupdated

--- lib/views/signup/signupPage.dart
+++ lib/views/signup/signupPage.dart
@@ -19,96 +19,101 @@ class SignupPage extends StatelessWidget {
             Align(
               alignment: Alignment.topCenter,
               child: Container(
+                height: ScreenHeight * 0.45,
+                width: ScreenWidth,
                 child: Image.asset(
                   'assets/images/login_sign_Background.png',
                   fit: BoxFit.fill,
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
+            Positioned(
+              top: ScreenHeight * 0.25,
               child: Hero(
                 tag: 'Login_SignUp',
                 child: Material(
                   type: MaterialType.transparency,
-                  child: Container(
-                    height: ScreenHeight * 0.71,
-                    width: ScreenWidth,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: SingleChildScrollView(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 20),
-                              child: AppBar(
-                                elevation: 0,
-                                shadowColor: Color(0x00),
-                                leading: IconButton(
-                                  padding: EdgeInsets.only(left: 20),
-                                  onPressed: () {
-                                    Get.back();
-                                  },
-                                  icon: Icon(Icons.arrow_back),
-                                  color: KTextColor,
-                                ),
-                                centerTitle: true,
-                                title: MyText(
-                                    text: 'Sign Up',
+                  child: SingleChildScrollView(
+                    child: Container(
+                      height: ScreenHeight * 0.71,
+                      width: ScreenWidth,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: SingleChildScrollView(
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 20),
+                                child: AppBar(
+                                  elevation: 0,
+                                  shadowColor: Color(0x00),
+                                  leading: IconButton(
+                                    padding: EdgeInsets.only(left: 20),
+                                    onPressed: () {
+                                      Get.back();
+                                    },
+                                    icon: Icon(Icons.arrow_back),
                                     color: KTextColor,
-                                    size: 25),
-                                backgroundColor: Color(0x00),
+                                  ),
+                                  centerTitle: true,
+                                  title: MyText(
+                                      text: 'Sign Up',
+                                      color: KTextColor,
+                                      size: 25),
+                                  backgroundColor: Color(0x00),
+                                ),
                               ),
-                            ),
-                            MyText(
-                              text: 'Email',
-                              color: KTextColor,
-                              size: 20,
-                              left: 30,
-                            ),
-                            MyTextFormField(
-                              top: 10,
-                              left: 20,
-                              right: 20,
-                              hintText: 'Enter your email.',
-                            ),
-                            MyText(
-                              text: 'Password',
-                              color: KTextColor,
-                              size: 20,
-                              top: 20,
-                              left: 30,
-                            ),
-                            MyTextFormField(
-                              top: 10,
-                              left: 20,
-                              right: 20,
-                              hintText: 'Enter your password.',
-                            ),
-                            MyText(
-                              text: 'Conform Password',
-                              color: KTextColor,
-                              size: 20,
-                              top: 20,
-                              left: 30,
-                            ),
-                            MyTextFormField(
-                              top: 10,
-                              left: 20,
-                              right: 20,
-                              bottom: 80,
-                              hintText: 'Enter your password again.',
-                            ),
-                            Center(
-                                child: MyButton(
-                              text: 'Login',
-                              ontap: () {},
-                              bottom: 20,
-                            )),
-                          ]),
+                              MyText(
+                                text: 'Email',
+                                color: KTextColor,
+                                size: 20,
+                                left: 30,
+                              ),
+                              MyTextFormField(
+                                top: 10,
+                                left: 20,
+                                right: 20,
+                                hintText: 'Enter your email.',
+                              ),
+                              MyText(
+                                text: 'Password',
+                                color: KTextColor,
+                                size: 20,
+                                top: 20,
+                                left: 30,
+                              ),
+                              MyTextFormField(
+                                top: 10,
+                                left: 20,
+                                right: 20,
+                                hintText: 'Enter your password.',
+                              ),
+                              MyText(
+                                text: 'Conform Password',
+                                color: KTextColor,
+                                size: 20,
+                                top: 20,
+                                left: 30,
+                              ),
+                              MyTextFormField(
+                                top: 10,
+                                left: 20,
+                                right: 20,
+                                bottom: 80,
+                                hintText: 'Enter your password again.',
+                              ),
+                              Center(
+                                  child: MyButton(
+                                text: 'Login',
+                                ontap: () {},
+                                bottom: 20,
+                              )),
+                            ]),
+                      ),
                     ),
                   ),
                 ),
