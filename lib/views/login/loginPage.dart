c128d81a34275ca0a7c301efe2e180e7392618d7
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-10-03 17:35:15 +0545

    quick update

--- lib/views/login/loginPage.dart
+++ lib/views/login/loginPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:mero_hostel/utils/constant.dart';
 import 'package:mero_hostel/views/signup/signupPage.dart';
@@ -20,95 +21,91 @@ class LoginPage extends StatelessWidget {
           children: [
             Align(
               alignment: Alignment.topCenter,
-              child: Container(
-                child: Image.asset(
-                  'assets/images/login_sign_Background.png',
-                  fit: BoxFit.fill,
+              child: SingleChildScrollView(
+                child: Container(
+                  child: Image.asset(
+                    'assets/images/login_sign_Background.png',
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
-              child: Hero(
-                tag: 'Login_SignUp',
-                child: Material(
-                  type: MaterialType.transparency,
-                  child: Container(
-                    height: ScreenHeight * 0.6,
-                    width: ScreenWidth,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Center(
-                            child: MyText(
-                              text: 'Login',
-                              color: KTextColor,
-                              size: 32,
-                              top: 15,
-                              bottom: 30,
-                            ),
-                          ),
-                          MyText(
-                            text: 'Email',
-                            color: KTextColor,
-                            size: 20,
-                            left: 30,
-                          ),
-                          MyTextFormField(
-                            top: 10,
-                            left: 20,
-                            right: 20,
-                            hintText: 'Enter your email.',
-                          ),
-                          MyText(
-                            text: 'Password',
-                            color: KTextColor,
-                            size: 20,
-                            top: 20,
-                            left: 30,
-                          ),
-                          MyTextFormField(
-                            top: 10,
-                            left: 20,
-                            right: 20,
-                            hintText: 'Enter your password.',
-                          ),
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: MyText(
-                                top: 10,
-                                right: 20,
-                                bottom: 20,
-                                text: 'Forgot Password?',
-                                ontap: () {},
-                                color: Color(0xff0E6A28),
-                                size: 18),
-                          ),
-                          Center(
-                              child: MyButton(
-                            text: 'Login',
-                            ontap: () {},
-                            bottom: 20,
-                          )),
-                          Center(
-                            child: GestureDetector(
-                              onTap: () {
-                                Get.to(() => SignupPage());
-                              },
-                              child: MyRichText(
-                                  text1: 'Don\'t have a account?  ',
-                                  text2: 'Sign up'),
-                            ),
-                          )
-                        ]),
-                  ),
+              child: Container(
+                height: ScreenHeight * 0.6,
+                width: ScreenWidth,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
                 ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: MyText(
+                          text: 'Login',
+                          color: KTextColor,
+                          size: 32,
+                          top: 15,
+                          bottom: 30,
+                        ),
+                      ),
+                      MyText(
+                        text: 'Email',
+                        color: KTextColor,
+                        size: 20,
+                        left: 30,
+                      ),
+                      MyTextFormField(
+                        top: 10,
+                        left: 20,
+                        right: 20,
+                        hintText: 'Enter your email.',
+                      ),
+                      MyText(
+                        text: 'Password',
+                        color: KTextColor,
+                        size: 20,
+                        top: 20,
+                        left: 30,
+                      ),
+                      MyTextFormField(
+                        top: 10,
+                        left: 20,
+                        right: 20,
+                        hintText: 'Enter your password.',
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: MyText(
+                            top: 10,
+                            right: 20,
+                            bottom: 20,
+                            text: 'Forgot Password?',
+                            ontap: () {},
+                            color: Color(0xff0E6A28),
+                            size: 18),
+                      ),
+                      Center(
+                          child: MyButton(
+                        text: 'Login',
+                        ontap: () {},
+                        bottom: 20,
+                      )),
+                      Center(
+                        child: GestureDetector(
+                          onTap: () {
+                            Get.to(() => SignupPage());
+                          },
+                          child: MyRichText(
+                              text1: 'Don\'t have a account?  ',
+                              text2: 'Sign up'),
+                        ),
+                      )
+                    ]),
               ),
-            )
+            ),
           ],
         ),
       ),
--- lib/views/signup/signupPage.dart
+++ lib/views/signup/signupPage.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mero_hostel/utils/constant.dart';
 import 'package:mero_hostel/widgets/Mytext.dart';
-import 'package:mero_hostel/widgets/myRichText.dart';
 import 'package:mero_hostel/widgets/myTextFormField.dart';
 import 'package:mero_hostel/widgets/mybutton.dart';
 
@@ -39,19 +38,78 @@ class SignupPage extends StatelessWidget {
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(50),
                     ),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Center(
-                            child: MyText(
-                              text: 'Login',
-                              color: KTextColor,
-                              size: 32,
-                              top: 15,
-                              bottom: 30,
+                    child: SingleChildScrollView(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 20),
+                              child: AppBar(
+                                elevation: 0,
+                                shadowColor: Color(0x00),
+                                leading: IconButton(
+                                  padding: EdgeInsets.only(left: 20),
+                                  onPressed: () {
+                                    Get.back();
+                                  },
+                                  icon: Icon(Icons.arrow_back),
+                                  color: KTextColor,
+                                ),
+                                centerTitle: true,
+                                title: MyText(
+                                    text: 'Sign Up',
+                                    color: KTextColor,
+                                    size: 25),
+                                backgroundColor: Color(0x00),
+                              ),
                             ),
-                          ),
-                        ]),
+                            MyText(
+                              text: 'Email',
+                              color: KTextColor,
+                              size: 20,
+                              left: 30,
+                            ),
+                            MyTextFormField(
+                              top: 10,
+                              left: 20,
+                              right: 20,
+                              hintText: 'Enter your email.',
+                            ),
+                            MyText(
+                              text: 'Password',
+                              color: KTextColor,
+                              size: 20,
+                              top: 20,
+                              left: 30,
+                            ),
+                            MyTextFormField(
+                              top: 10,
+                              left: 20,
+                              right: 20,
+                              hintText: 'Enter your password.',
+                            ),
+                            MyText(
+                              text: 'Conform Password',
+                              color: KTextColor,
+                              size: 20,
+                              top: 20,
+                              left: 30,
+                            ),
+                            MyTextFormField(
+                              top: 10,
+                              left: 20,
+                              right: 20,
+                              bottom: 80,
+                              hintText: 'Enter your password again.',
+                            ),
+                            Center(
+                                child: MyButton(
+                              text: 'Login',
+                              ontap: () {},
+                              bottom: 20,
+                            )),
+                          ]),
+                    ),
                   ),
                 ),
               ),
