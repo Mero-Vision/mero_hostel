8a4fe3a5723a9cd514d29c3cfda7a87133fd3d61
Hancie123 <hanciewanemphago@gmail.com>
2023-10-05 17:11:19 +0545

    login design updated

--- lib/views/login/loginPage.dart
+++ lib/views/login/loginPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mero_hostel/utils/constant.dart';
 import 'package:get/get.dart';
 import 'package:mero_hostel/controller/loginController.dart';
@@ -20,136 +21,270 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     LoginController controller = Get.put(LoginController());
     return Scaffold(
-      backgroundColor: Color(0xff698AFF),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: ScreenHeight, // Set a fixed height
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                height: ScreenHeight * 0.1,
-              ),
-              Image.asset(
+        backgroundColor: Color(0xff698AFF),
+        body: Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+            ),
+            Positioned(
+              top: 50,
+              left: 0,
+              right: 0,
+              child: Image.asset(
                 'assets/icons/loginlogo.png',
-                height: ScreenHeight * 0.3,
-                width: 270,
-                fit: BoxFit.fill,
+                height: 250,
+                width: 200,
+                fit: BoxFit.contain,
               ),
-              Expanded(
+            ),
+            Positioned(
+                bottom: 0,
                 child: Container(
-                  width: ScreenWidth,
+                  height: 500,
+                  width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50),
-                    ),
-                  ),
-                  child: Form(
-                    key: _formkey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: MyText(
-                            text: 'Login',
-                            color: KTextColor,
-                            size: 32,
-                            top: 15,
-                            bottom: 30,
-                          ),
-                        ),
-                        MyText(
-                          text: 'Email',
-                          color: KTextColor,
-                          size: 20,
-                          left: 30,
-                        ),
-                        MyTextFormField(
-                          controller: EmailController,
-                          top: 10,
-                          left: 20,
-                          right: 20,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Email is required!';
-                            }
-                            return null;
-                          },
-                          hintText: 'Enter your email.',
-                        ),
-                        MyText(
-                          text: 'Password',
-                          color: KTextColor,
-                          size: 20,
-                          top: 20,
-                          left: 30,
-                        ),
-                        MyTextFormField(
-                          controller: PasswordController,
-                          top: 10,
-                          left: 20,
-                          right: 20,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Password is required!';
-                            }
-                            return null;
-                          },
-                          hintText: 'Enter your password.',
-                        ),
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: MyText(
-                            top: 10,
-                            right: 20,
-                            bottom: 20,
-                            text: 'Forgot Password?',
-                            ontap: () {},
-                            color: Color(0xff0E6A28),
-                            size: 18,
-                          ),
-                        ),
-                        Center(
-                          child: MyButton(
-                            text: 'Login',
-                            ontap: () {
-                              if (_formkey.currentState!.validate()) {
-                                controller.login(
-                                  EmailController.text.toString(),
-                                  PasswordController.text.toString(),
-                                );
-                              }
-                            },
-                            bottom: 20,
-                          ),
-                        ),
-                        Center(
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                  pageBuilder: (context, a, b) => SignupPage(),
-                                ),
-                              );
-                            },
-                            child: MyRichText(
-                              text1: 'Don\'t have an account?  ',
-                              text2: 'Sign up',
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(50),
+                      )),
+                  child: Column(
+                    children: [
+                      Form(
+                        key: _formkey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: MyText(
+                                text: 'Login',
+                                color: KTextColor,
+                                size: 32,
+                                top: 15,
+                                bottom: 30,
+                              ),
                             ),
-                          ),
+                            MyText(
+                              text: 'Email',
+                              color: KTextColor,
+                              size: 20,
+                              left: 30,
+                            ),
+                            MyTextFormField(
+                              controller: EmailController,
+                              top: 10,
+                              left: 20,
+                              right: 20,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Email is required!';
+                                }
+                                return null;
+                              },
+                              hintText: 'Enter your email.',
+                            ),
+                            MyText(
+                              text: 'Password',
+                              color: KTextColor,
+                              size: 20,
+                              top: 20,
+                              left: 30,
+                            ),
+                            MyTextFormField(
+                              controller: PasswordController,
+                              top: 10,
+                              left: 20,
+                              right: 20,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Password is required!';
+                                }
+                                return null;
+                              },
+                              hintText: 'Enter your password.',
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: MyText(
+                                top: 10,
+                                right: 20,
+                                bottom: 20,
+                                text: 'Forgot Password?',
+                                ontap: () {},
+                                color: Color(0xff0E6A28),
+                                size: 18,
+                              ),
+                            ),
+                            Center(
+                              child: MyButton(
+                                text: 'Login',
+                                ontap: () {
+                                  if (_formkey.currentState!.validate()) {
+                                    controller.login(
+                                      EmailController.text.toString(),
+                                      PasswordController.text.toString(),
+                                    );
+                                  }
+                                },
+                                bottom: 20,
+                              ),
+                            ),
+                            Center(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (context, a, b) =>
+                                          SignupPage(),
+                                    ),
+                                  );
+                                },
+                                child: MyRichText(
+                                  text1: 'Don\'t have an account?  ',
+                                  text2: 'Sign up',
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                ))
+          ],
+        )
+
+        // SingleChildScrollView(
+        //   child: SizedBox(
+        //     height: ScreenHeight, // Set a fixed height
+        //     child: Column(
+        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //       children: [
+        //         SizedBox(
+        //           height: ScreenHeight * 0.1,
+        //         ),
+        //         Image.asset(
+        //           'assets/icons/loginlogo.png',
+        //           height: ScreenHeight * 0.3,
+        //           width: 270,
+        //           fit: BoxFit.fill,
+        //         ),
+        //         Expanded(
+        //           child: Container(
+        //             width: ScreenWidth,
+        //             decoration: BoxDecoration(
+        //               color: Colors.white,
+        //               borderRadius: BorderRadius.only(
+        //                 topLeft: Radius.circular(50),
+        //                 topRight: Radius.circular(50),
+        //               ),
+        //             ),
+        //             child: Form(
+        //               key: _formkey,
+        //               child: Column(
+        //                 crossAxisAlignment: CrossAxisAlignment.start,
+        //                 children: [
+        //                   Center(
+        //                     child: MyText(
+        //                       text: 'Login',
+        //                       color: KTextColor,
+        //                       size: 32,
+        //                       top: 15,
+        //                       bottom: 30,
+        //                     ),
+        //                   ),
+        //                   MyText(
+        //                     text: 'Email',
+        //                     color: KTextColor,
+        //                     size: 20,
+        //                     left: 30,
+        //                   ),
+        //                   MyTextFormField(
+        //                     controller: EmailController,
+        //                     top: 10,
+        //                     left: 20,
+        //                     right: 20,
+        //                     validator: (value) {
+        //                       if (value!.isEmpty) {
+        //                         return 'Email is required!';
+        //                       }
+        //                       return null;
+        //                     },
+        //                     hintText: 'Enter your email.',
+        //                   ),
+        //                   MyText(
+        //                     text: 'Password',
+        //                     color: KTextColor,
+        //                     size: 20,
+        //                     top: 20,
+        //                     left: 30,
+        //                   ),
+        //                   MyTextFormField(
+        //                     controller: PasswordController,
+        //                     top: 10,
+        //                     left: 20,
+        //                     right: 20,
+        //                     validator: (value) {
+        //                       if (value!.isEmpty) {
+        //                         return 'Password is required!';
+        //                       }
+        //                       return null;
+        //                     },
+        //                     hintText: 'Enter your password.',
+        //                   ),
+        //                   Align(
+        //                     alignment: Alignment.topRight,
+        //                     child: MyText(
+        //                       top: 10,
+        //                       right: 20,
+        //                       bottom: 20,
+        //                       text: 'Forgot Password?',
+        //                       ontap: () {},
+        //                       color: Color(0xff0E6A28),
+        //                       size: 18,
+        //                     ),
+        //                   ),
+        //                   Center(
+        //                     child: MyButton(
+        //                       text: 'Login',
+        //                       ontap: () {
+        //                         if (_formkey.currentState!.validate()) {
+        //                           controller.login(
+        //                             EmailController.text.toString(),
+        //                             PasswordController.text.toString(),
+        //                           );
+        //                         }
+        //                       },
+        //                       bottom: 20,
+        //                     ),
+        //                   ),
+        //                   Center(
+        //                     child: GestureDetector(
+        //                       onTap: () {
+        //                         Navigator.push(
+        //                           context,
+        //                           PageRouteBuilder(
+        //                             pageBuilder: (context, a, b) => SignupPage(),
+        //                           ),
+        //                         );
+        //                       },
+        //                       child: MyRichText(
+        //                         text1: 'Don\'t have an account?  ',
+        //                         text2: 'Sign up',
+        //                       ),
+        //                     ),
+        //                   ),
+        //                 ],
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
+        );
   }
 }
